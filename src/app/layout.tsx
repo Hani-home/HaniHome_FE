5d48fa96b53c62f763b33d6b134d16dce152a35d
chaeyoungwon <epopcy@naver.com>
2025-05-22 02:49:45 +0900

    chore: typography 설정 방식 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,15 @@
 import type { Metadata } from "next";
+import localFont from "next/font/local";
 
 import "@/styles/globals.css";
+
+const suit = localFont({
+  src: "../../public/fonts/SUIT-Variable.woff2",
+  display: "swap",
+  style: "normal",
+  weight: "45 920",
+  variable: "--font-suit",
+});
 
 export const metadata: Metadata = {
   title: "하니홈",
@@ -24,7 +33,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ko">
+    <html lang="ko" className={suit.className}>
       <body>
         <div className="mx-auto min-h-screen max-w-[768px] min-w-[375px] pt-12">
           {children}
