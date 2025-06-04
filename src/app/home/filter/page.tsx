89bb7f30abe01dc03ca007761df3d52e86e9060b
chaeyoungwon <epopcy@naver.com>
2025-06-04 13:17:26 +0900

    feat: 필터 페이지 추가

---
+++ src/app/home/filter/page.tsx
@@ -0,0 +1,11 @@
+import BackHeader from "@/components/layout/header/BackHeader";
+
+const Filter = () => {
+  return (
+    <div className="scrollbar-hide flex h-screen flex-col overflow-y-auto">
+      <BackHeader />
+    </div>
+  );
+};
+
+export default Filter;
