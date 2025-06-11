3b1e573a7c5ea31a909b77e783f3fc92fca80811
chaeyoungwon <epopcy@naver.com>
2025-06-12 01:37:20 +0900

    design: leading-normal 추가

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -6,7 +6,7 @@ const NotFound = () => {
     <div className="flex min-h-screen flex-col items-center justify-center gap-37">
       <div className="flex flex-col items-center justify-center gap-8">
         <Title />
-        <div className="text-body2-med flex flex-col items-center justify-center text-gray-900">
+        <div className="text-body2-med flex flex-col items-center justify-center leading-normal text-gray-900">
           <p>페이지를 찾을 수 없어요. </p>
           <p>존재하지 않는 주소를 입력했거나, </p>
           <p>요청한 페이지의 주소가 변경, 삭제되었어요.</p>
