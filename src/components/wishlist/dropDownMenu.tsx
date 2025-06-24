3d1e8a6bb2ab6c2fd176844bc35dc4331ac00d18
lemoncurdyogurt <80269181+lemoncurdyogurt@users.noreply.github.com>
2025-06-24 15:22:14 +0900

    feat: 드롭다운 메뉴츄가

---
+++ src/components/wishlist/dropDownMenu.tsx
@@ -0,0 +1,27 @@
+import Divider from "@/components/common/Divider";
+
+interface DropDownMenuProps {
+  onSelect: (order: "latest" | "popular") => void;
+}
+const DropDownMenu = ({ onSelect }: DropDownMenuProps) => {
+  return (
+    <div className="text-lab1-sb h-[86px] w-[70px] border border-gray-300 bg-white px-4 py-3 text-gray-800">
+      <div className="flex flex-col gap-[8px]">
+        <button
+          className="cursor-pointer pb-1"
+          onClick={() => onSelect("latest")}
+        >
+          최신순
+        </button>
+        <Divider />
+        <button
+          className="cursor-pointer pt-1"
+          onClick={() => onSelect("popular")}
+        >
+          인기순
+        </button>
+      </div>
+    </div>
+  );
+};
+export default DropDownMenu;
