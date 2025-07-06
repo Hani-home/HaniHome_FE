ee25266ca743f2da9f0a6f2003860bbb94bc5304
lemoncurdyogurt <80269181+lemoncurdyogurt@users.noreply.github.com>
2025-07-06 22:59:49 +0900

    feat: 내놓은 매물 페이지

--- src/app/mypage/listings/page.tsx
+++ src/app/mypage/listings/page.tsx
@@ -1,11 +1,48 @@
-// import SelectTab from "@/components/common/SelectTab";
+"use client";
+
+import { useRouter } from "next/navigation";
+
+import { useState } from "react";
+
+import SelectTab from "@/components/common/SelectTab";
+import ListingCard from "@/components/home/ListingCard";
 import BackHeader from "@/components/layout/header/BackHeader";
 
+import { ListingDummies } from "@/constants/mock/listing-card-dummies";
+
+const TABS = [
+  { label: "거래중", key: "active" },
+  { label: "거래완료", key: "completed" },
+  { label: "숨김", key: "hidden" },
+];
+
 const Listings = () => {
+  const router = useRouter();
+  const [activeTab, setActiveTab] = useState("active");
+
+  const filteredListings = ListingDummies.filter(item => {
+    if (activeTab === "active") return item.status === "거래 중";
+    if (activeTab === "completed") return item.status === "거래 완료";
+    if (activeTab === "hidden") return item.status === "숨김";
+    return true;
+  });
+
   return (
     <div>
       <BackHeader />
-      {/* <SelectTab tabs={["거래중: ing", "거래완료: end", "숨김: hide"]} activeTab="거래중" onChange={}/> */}
+      <SelectTab tabs={TABS} activeTab={activeTab} onChange={setActiveTab} />
+
+      {filteredListings.map(item => (
+        <div onClick={()=>router.push( `/listings/${item.id}/edit`)}>
+          <ListingCard
+            key={item.id}
+            {...item}
+            isLiked={false}
+            onToggleLike={() => {}}
+            heartColor="text-gray"
+          />
+        </div>
+      ))}
     </div>
   );
 };
