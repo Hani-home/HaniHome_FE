be167240c23ce9d439a3a40bc7fba7f8602149a7
chaeyoungwon <epopcy@naver.com>
2025-07-08 01:28:08 +0900

    design: 드롭다운 스타일 수정

--- src/components/listings/DropDownMenu.tsx
+++ src/components/listings/DropDownMenu.tsx
@@ -30,35 +30,37 @@ const DropDownMenu = ({ selectedKey, onSelect }: DropDownMenuProps) => {
   };
 
   return (
-    <div className="text-body1-med relative w-[109px]">
-      <div className="rounded-[4px] border border-gray-600 bg-white text-gray-800">
+    <div className="text-body1-med relative mx-4 w-[109px]">
+      <div
+        className={`rounded-[4px] border bg-white text-gray-800 ${
+          selectedKey === "active" ? "border-gray-400" : "border-gray-600"
+        }`}
+      >
         <div
           onClick={handleDropdown}
           className="flex h-[36px] cursor-pointer items-center justify-between px-3"
         >
           <span>{selectedTab?.label}</span>
           <DownArrow
-            className={`h-[18px] w-[18px] transition-transform duration-200 ${
-              isOpen ? "rotate-180" : ""
-            }`}
+            className={`h-[18px] w-[18px] ${isOpen ? "rotate-180" : ""}`}
           />
         </div>
         {/* 열림 상태 */}
         {isOpen && (
-          <div className="absolute top-0 left-0 z-10 w-full rounded-[4px] border border-gray-600 bg-white">
+          <div className="absolute top-0 left-0 z-10 flex w-full flex-col gap-2 rounded-[4px] border border-gray-600 bg-white py-[6px]">
             <div
-              className="flex cursor-pointer items-center justify-between px-3 py-[6px] text-gray-800"
+              className="flex cursor-pointer items-center justify-between px-3 text-gray-800"
               onClick={handleDropdown}
             >
               {selectedTab?.label}
-              <DownArrow className="h-[18px] w-[18px] rotate-180 transition-transform duration-200" />
+              <DownArrow className="h-[18px] w-[18px] rotate-180" />
             </div>
 
             {unselectedTabs.map(tab => (
               <div
                 key={tab.key}
                 onClick={() => handleSelect(tab.key)}
-                className="hover:text-mint cursor-pointer px-3 py-[6px] text-gray-800"
+                className="hover:text-mint cursor-pointer px-3 text-gray-800"
               >
                 {tab.label}
               </div>
