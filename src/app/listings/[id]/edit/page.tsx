35a40a335b9961517e419562c40e6861717e0781
lemoncurdyogurt <80269181+lemoncurdyogurt@users.noreply.github.com>
2025-07-08 17:50:58 +0900

    feat: 매물 수정 및 매물등록확인 UI title section 컴포넌트 생성 및 추가

--- src/app/listings/[id]/edit/page.tsx
+++ src/app/listings/[id]/edit/page.tsx
@@ -1,4 +1,17 @@
-const ListingsEdit = () =>{
-  return <>매물정보 수정</>
-}
+import BottomActionBar from "@/components/common/BottomActionBar";
+import BackHeader from "@/components/layout/header/BackHeader";
+import TitleSection from "@/components/listings/detailShow/TitleSection";
+
+const ListingsEdit = () => {
+  return (
+    <>
+      <BackHeader />
+      <TitleSection
+        title="수정할 정보 섹션을 클릭해주세요"
+        label="추가로 변경해야하는 섹션도 수정해주세요"
+      />
+      <BottomActionBar label="저장"/>
+    </>
+  );
+};
 export default ListingsEdit;
