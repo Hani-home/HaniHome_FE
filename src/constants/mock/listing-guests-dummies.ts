3a5e7718b8cb6b01dca366d6823c50512c4c9cd7
lemoncurdyogurt <80269181+lemoncurdyogurt@users.noreply.github.com>
2025-07-08 13:09:47 +0900

    test: 거래한 게스트 목록 목데이터 추가

---
+++ src/constants/mock/listing-guests-dummies.ts
@@ -0,0 +1,142 @@
+export const listingGuests = [
+  {
+    listingsId: 1,
+    guests: [
+      {
+        nickname: "suding1",
+        date: "2025-07-12",
+        time: "14:00",
+      },
+      {
+        nickname: "suding2",
+        date: "2025-07-12",
+        time: "15:00",
+      },
+      {
+        nickname: "suding3",
+        date: "2025-07-12",
+        time: "16:00",
+      },
+      {
+        nickname: "suding4",
+        date: "2025-07-13",
+        time: "11:00",
+      },
+      {
+        nickname: "suding5",
+        date: "2025-07-13",
+        time: "12:00",
+      },
+      {
+        nickname: "suding6",
+        date: "2025-07-13",
+        time: "13:00",
+      },
+    ],
+  },
+  {
+    listingsId: 2,
+    guests: [
+      {
+        nickname: "suding1",
+        date: "2025-07-12",
+        time: "14:00",
+      },
+      {
+        nickname: "suding2",
+        date: "2025-07-12",
+        time: "15:00",
+      },
+      {
+        nickname: "suding3",
+        date: "2025-07-12",
+        time: "16:00",
+      },
+      {
+        nickname: "suding4",
+        date: "2025-07-13",
+        time: "11:00",
+      },
+      {
+        nickname: "suding5",
+        date: "2025-07-13",
+        time: "12:00",
+      },
+      {
+        nickname: "suding6",
+        date: "2025-07-13",
+        time: "13:00",
+      },
+    ],
+  },
+  {
+    listingsId: 3,
+    guests: [
+      {
+        nickname: "suding1",
+        date: "2025-07-12",
+        time: "14:00",
+      },
+      {
+        nickname: "suding2",
+        date: "2025-07-12",
+        time: "15:00",
+      },
+      {
+        nickname: "suding3",
+        date: "2025-07-12",
+        time: "16:00",
+      },
+      {
+        nickname: "suding4",
+        date: "2025-07-13",
+        time: "11:00",
+      },
+      {
+        nickname: "suding5",
+        date: "2025-07-13",
+        time: "12:00",
+      },
+      {
+        nickname: "suding6",
+        date: "2025-07-13",
+        time: "13:00",
+      },
+    ],
+  },
+  {
+    listingsId: 4,
+    guests: [
+      {
+        nickname: "suding1",
+        date: "2025-07-12",
+        time: "14:00",
+      },
+      {
+        nickname: "suding2",
+        date: "2025-07-12",
+        time: "15:00",
+      },
+      {
+        nickname: "suding3",
+        date: "2025-07-12",
+        time: "16:00",
+      },
+      {
+        nickname: "suding4",
+        date: "2025-07-13",
+        time: "11:00",
+      },
+      {
+        nickname: "suding5",
+        date: "2025-07-13",
+        time: "12:00",
+      },
+      {
+        nickname: "suding6",
+        date: "2025-07-13",
+        time: "13:00",
+      },
+    ],
+  },
+];
