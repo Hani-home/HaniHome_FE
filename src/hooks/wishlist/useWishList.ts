54510accbe8129f34f35d5e77025a49eafc88ad5
chaeyoungwon <epopcy@naver.com>
2025-07-25 20:14:09 +0900

    feat: 즐겨찾기 매물 조회 연동 및 useToggleWish 함수 작성

--- src/hooks/wishlist/useWishList.ts
+++ src/hooks/wishlist/useWishList.ts
@@ -1,7 +1,14 @@
 import { useAuthStore } from "@/stores/useAuthStore";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 
-import { getMyWishList } from "@/apis/wishlist";
+import {
+  addWish,
+  getMyWishList,
+  getWishedProperties,
+  removeWish,
+} from "@/apis/wishlist";
+
+import { WishListSortType } from "@/types/wishlist";
 
 export const useWishList = () => {
   const isLoggedIn = useAuthStore(state => state.isLoggedIn);
@@ -10,5 +17,31 @@ export const useWishList = () => {
     queryKey: ["wishList"],
     queryFn: getMyWishList,
     enabled: isLoggedIn,
+  });
+};
+
+export const useWishedProperties = (sort: WishListSortType = "latest") => {
+  return useQuery({
+    queryKey: ["wishList", sort],
+    queryFn: () => getWishedProperties(sort),
+  });
+};
+
+export const useToggleWish = (
+  refetchKey: (string | number)[] = ["wishList"],
+) => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: async ({ id, isLiked }: { id: number; isLiked: boolean }) => {
+      if (isLiked) {
+        await removeWish(id);
+      } else {
+        await addWish(id);
+      }
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: refetchKey });
+    },
   });
 };
