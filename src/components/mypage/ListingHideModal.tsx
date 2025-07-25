09e2f6cc3f653133054d49f60c1ad54021e24f0d
chaeyoungwon <epopcy@naver.com>
2025-07-25 12:21:34 +0900

    refactor: description 배열 형태로 수정

--- src/components/mypage/ListingHideModal.tsx
+++ src/components/mypage/ListingHideModal.tsx
@@ -8,15 +8,11 @@ const ListingHideModal = ({ onClose }: ListingHideModalProps) => {
   return (
     <AlertModal
       title="이 매물에 확정된 뷰잉 예약이 있어요"
-      description={
-        <>
-          이 상세페이지를 숨기면, 뷰잉을 예약한 게스트가
-          <br />
-          상세페이지를 볼 수 없게 되어 문제가 발생할 수 있어요.
-          <br />
-          숨김 처리와 함께 해당 예약을 취소하시겠어요?
-        </>
-      }
+      description={[
+        "이 상세페이지를 숨기면, 뷰잉을 예약한 게스트가",
+        "상세페이지를 볼 수 없게 되어 문제가 발생할 수 있어요.",
+        "숨김 처리와 함께 해당 예약을 취소하시겠어요?",
+      ]}
       onClose={onClose}
       actionLabel="예약 취소 후 숨기기"
       onActionClick={() => {
