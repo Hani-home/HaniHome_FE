3e301349505ed76fc1a17bcb3bf921b1cb608139
chaeyoungwon <epopcy@naver.com>
2025-07-26 20:11:30 +0900

    feat: 입주 조건 탭 api 연동

--- src/components/listings/MoveInCondition.tsx
+++ src/components/listings/MoveInCondition.tsx
@@ -1,18 +1,52 @@
+import { formatToDateRange } from "@/utils/dateFormatter";
+
 import InfoCard from "@/components/listings/shared/InfoCard";
 import Label from "@/components/listings/shared/Label";
 import Section from "@/components/listings/shared/Section";
 
+import {
+  CAPACITY_RENT_LABEL_MAP,
+  CAPACITY_SHARE_MAP,
+} from "@/constants/capacity-options";
+import { GENDER_PREFERENCE_LABEL_MAP } from "@/constants/gender-options";
+
+import { Property } from "@/types/property";
+
 import ExtraConditions from "./ExtraConditions";
 
-const MoveInCondition = () => {
+const MoveInCondition = ({ data }: { data: Property }) => {
   return (
     <div className="bg-gray-0 flex flex-col py-3">
       <Section>
         <div className="flex items-center justify-between px-4 py-3">
           <Label>거주 가능 조건</Label>
           <div className="text-body2-med flex flex-col items-end gap-1 text-gray-700">
-            <span>성별 무관</span>
-            <span>최대 3인 1실</span>
+            <span>
+              {
+                GENDER_PREFERENCE_LABEL_MAP[
+                  data.genderPreference as keyof typeof GENDER_PREFERENCE_LABEL_MAP
+                ]
+              }
+              {data.lgbtAvailable && " / LGBTQIA+"}
+            </span>
+            {data.kind === "SHARE" && (
+              <span>
+                {
+                  CAPACITY_SHARE_MAP[
+                    data.capacityShare as keyof typeof CAPACITY_SHARE_MAP
+                  ]
+                }
+              </span>
+            )}
+            {data.kind === "RENT" && (
+              <span>
+                {
+                  CAPACITY_RENT_LABEL_MAP[
+                    data.capacityRent as keyof typeof CAPACITY_RENT_LABEL_MAP
+                  ]
+                }
+              </span>
+            )}
           </div>
         </div>
       </Section>
@@ -24,7 +58,7 @@ const MoveInCondition = () => {
             title="노티스"
             items={[
               <span className="text-body2-med text-gray-700" key="notice">
-                최소 n주 전
+                최소 {data.livingConditions.noticePeriodWeeks}주 전
               </span>,
             ]}
           />
@@ -33,7 +67,7 @@ const MoveInCondition = () => {
             title="최소 거주 기간"
             items={[
               <span className="text-body2-med text-gray-700" key="minPeriod">
-                최소 n주
+                최소 {data.livingConditions.minimumStayWeeks}주
               </span>,
             ]}
           />
@@ -44,7 +78,10 @@ const MoveInCondition = () => {
         <div className="flex flex-col gap-2 px-4 py-3">
           <Label>입주 가능일</Label>
           <div className="text-body2-med flex text-gray-700">
-            25년 5월 nn일<span>&nbsp;-&nbsp;</span>25년 6월 nn일
+            {formatToDateRange(
+              data.moveInInfo.availableFrom,
+              data.moveInInfo.availableTo,
+            )}
           </div>
         </div>
       </Section>
@@ -53,9 +90,9 @@ const MoveInCondition = () => {
         <div className="flex flex-col gap-3 px-4 py-3">
           <div className="flex items-center justify-between">
             <Label>추가 고려사항</Label>
-            <span className="text-body2-med text-gray-600">조건 협의 가능</span>
+            <span className="text-cap1-med text-gray-600">조건 협의 가능</span>
           </div>
-          <ExtraConditions />
+          <ExtraConditions data={data.optionItems} />
         </div>
       </Section>
     </div>
