8d88ab0130b3a8cb23de0976d390dc2ced63fb2d
lemoncurdyogurt <80269181+lemoncurdyogurt@users.noreply.github.com>
2025-08-06 17:30:29 +0900

    chore: funnel 단계 주석처리 취소

--- src/components/listings/create/addressPhoto/AddressField.tsx
+++ src/components/listings/create/addressPhoto/AddressField.tsx
@@ -265,7 +265,7 @@ const AddressField = ({ onNext, edit }: AddressFieldProps) => {
         </>
       )}
 
-      {isSearchClicked && (
+      {isSearchClicked && selectedAddress && (
         <>
           <div className="pb-[70px]">
             <div className="flex flex-col gap-2">
